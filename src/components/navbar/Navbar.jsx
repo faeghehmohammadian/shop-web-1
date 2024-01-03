672ad1a862aaada3af98249ed9ce5a7635eb2c0e
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2024-01-03 23:24:33 +0400

    responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,43 @@
 import styled from './navbar.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faGooglePlus, faBehance} from '@fortawesome/free-brands-svg-icons';
-import {faBagShopping,faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';  
-//import { faPipe } from '@fortawesome/pro-solid-svg-icons';
+import { faFacebookF, faTwitter, faGooglePlus, faBehance } from '@fortawesome/free-brands-svg-icons';
+import { faBagShopping, faMagnifyingGlass, faBars, faShare } from '@fortawesome/free-solid-svg-icons';
+
 function Navbar() {
     return (
         <div className={styled.header}>
             <div className={styled.leftSide}>
-                <ul>
-                    <li style={{ color: 'blue' , fontSize: '20px', fontWeight: 'bold' , marginRight:"40px"}}>Shopi</li>
-                    <li>HOME</li>
-                    <li>POST FORMATS</li>
-                    <li>PAGES</li>
-                    <li>PURCHASE</li>
-                </ul>
+                <li className={styled.shopi}>Shopi</li>
+                <div className={styled.menu}>
+                    <ul>
+                        <li>HOME</li>
+                        <li>POST FORMATS</li>
+                        <li>PAGES</li>
+                        <li>PURCHASE</li>
+                    </ul>
+                    <div className={styled.mobileMenu}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
+                </div>
+
             </div>
 
             <div className={styled.rightSide}>
-                <ul>
-                    <li><FontAwesomeIcon icon={faFacebookF} /></li>
-                    <li><FontAwesomeIcon icon={faTwitter} /></li>
-                    <li><FontAwesomeIcon icon={faGooglePlus} /></li>
-                    <li><FontAwesomeIcon icon={faBehance} /></li>
-                </ul>
+                <div className={styled.socialIcon}>
+                    <ul>
+                        <li><FontAwesomeIcon icon={faFacebookF} /></li>
+                        <li><FontAwesomeIcon icon={faTwitter} /></li>
+                        <li><FontAwesomeIcon icon={faGooglePlus} /></li>
+                        <li><FontAwesomeIcon icon={faBehance} /></li>
+                    </ul>
+                    <div className={styled.socialIconMobile}>
+                        <FontAwesomeIcon icon={faShare} />
+                    </div>
+                </div>
+
                 <ul>
                     <li className={styled.hasCirle}>
-                         <FontAwesomeIcon icon={faBagShopping}/>     
+                        <FontAwesomeIcon icon={faBagShopping} />
                     </li>
                     <li>|</li>
                     <li><FontAwesomeIcon icon={faMagnifyingGlass} /></li>
